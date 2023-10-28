1f50a4f196fd43a0d0af2bd0db3076a49a172406
Zannatul Ferdousi Hema <shanzidadisha1234.com>
2023-10-28 23:34:55 +0600

    services added

--- car-doctor-client/src/layout/Service.jsx
+++ car-doctor-client/src/layout/Service.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { BsArrowRight } from "react-icons/bs";
 
 const Service = () => {
   const [services, setServices] = useState([]);
@@ -9,17 +10,40 @@ const Service = () => {
       .then((data) => setServices(data));
   }, []);
   return (
-    <div>
+    <div className="container mx-auto">
       <div className="mt-20 text-center w-1/2  mx-auto">
         <span className="font-medium text-[#FF3811]">Service</span>
-        <h1 className="text-5xl font-bold">
-        Our Service Area
-        </h1>
+        <h1 className="text-5xl font-bold">Our Service Area</h1>
         <p className="py-4 capitalize text-[#737373]">
-        the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. 
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised words which don't look even slightly believable.
         </p>
       </div>
-      <h1>service length: {services.length}</h1>
+
+      <div className="grid grid-cols-3 gap-5">
+        {services.map((service) => (
+          <div className="card bg-base-100 shadow-xl rounded-md">
+            <figure className="px-6 pt-6">
+              <img src={service.img} alt="Shoes" className="rounded-md h-52" />
+            </figure>
+            <div className="card-body ">
+              <h2 className="card-title text-lg font-bold">{service.title}</h2>
+              <div className="flex justify-between items-center">
+                <p className="font-medium text-[#FF3811]">
+                  Price: ${service.price}
+                </p>
+                <BsArrowRight className="text-[#FF3811]"></BsArrowRight>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="text-center  my-10">
+        <button className="btn btn-outline  border-[#FF3811] text-[#FF3811] rounded-md">
+          More Service
+        </button>
+      </div>
     </div>
   );
 };
